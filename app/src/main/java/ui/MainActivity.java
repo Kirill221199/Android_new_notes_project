3d11e86491a2ded1d5f45211f4d07e1a530cced1
Кирилл <agarin221199@mail.ru>
2022-02-23 17:32:28 +0500

    Initial commit

---
+++ app/src/main/java/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.kirill.android_new_notes_project.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
